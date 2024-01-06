dac811aa6789377249001ceab423d394d9f8a3e1
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-06 15:28:43 +0200

    New commit

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div>
-      
+       <h1>Balolebwami</h1>
     </div>
   )
 }
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from './components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className='max-w-6xl mx-auto'>
+        <main className='max-w-6xl mx-auto border'>
+          <Navbar />
         {children}
         </main>
         </body>
