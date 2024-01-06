82ef50cf3087ac6e7732d67009ac1c4dff5a5dc8
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-06 16:13:24 +0200

    new adding

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -6,11 +6,15 @@ const [isOpen, setIsOpen] = useState(false)
 const myMenu = [
     {name: 'Home', Links: 'home'},
     {name: 'About Us', Links: 'about'},
+    {name: 'Program', Links: 'Program'},
+    {name: 'Memberships', Links: 'Memberships'},
+    {name: 'Testimonios', Links: 'Testimonios'},
+    {name: 'Contact', Links: 'Contact'},
 ]
  
 
   return (
-    <nav className=''>
+    <nav className='w-full'>
       
     </nav>
   )
