1bee54a29dbb2920aeda3e7805c82e2680c63b68
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-07 16:56:48 +0200

    new code commit

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,31 +1,37 @@
-"use client"
-import React ,{useState} from 'react'
+"use client";
+import React, { useState } from "react";
 import { IoFitness } from "react-icons/io5";
-
+import Link from "next/link";
 
 export default function Navbar() {
-const [isOpen, setIsOpen] = useState(false)
-const myMenu = [
-    {name: 'Home', Links: 'home'},
-    {name: 'About Us', Links: 'about'},
-    {name: 'Program', Links: 'Program'},
-    {name: 'Memberships', Links: 'Memberships'},
-    {name: 'Testimonios', Links: 'Testimonios'},
-    {name: 'Contact', Links: 'Contact'},
-]
- 
+  const [isOpen, setIsOpen] = useState(false);
+  const myMenu = [
+    { name: "Home", Links: "home" },
+    { name: "About Us", Links: "about" },
+    { name: "Program", Links: "Program" },
+    { name: "Memberships", Links: "Memberships" },
+    { name: "Testimonios", Links: "Testimonios" },
+    { name: "Contact", Links: "Contact" },
+  ];
 
   return (
-    <nav className='w-full'>
-     <div className='flex justify-between items-center'>
-        <div className="flex gap-2">
-         <IoFitness className='text-3xl text-red-500'/>
-         <h1>fitness room</h1>
+    <nav className="w-full">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-2 items-center">
+          <IoFitness className="text-3xl text-slate-900" />
+          <h1>Fitness room</h1>
         </div>
-     </div>
 
-
-
+        <div className="">
+          <ul className="flex items-center  gap-3 font-medium text-sm ">
+            {myMenu.map((item, index) => (
+              <li className="myli" key={index}>
+                <Link href={item.Links}>{item.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
