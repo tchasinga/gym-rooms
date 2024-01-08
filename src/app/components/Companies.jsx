5656772a49dc3a1c87b72826ff06ba7f8dc8fb62
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-08 16:49:30 +0200

    new code is added

--- src/app/components/Companies.jsx
+++ src/app/components/Companies.jsx
@@ -2,8 +2,32 @@ import React from 'react'
 
 export default function Companies() {
   return (
-    <div>
-      <h1>My new code is added</h1>
+    <div className=''>
+
+      <div className="myback">
+        <div className="max-w-6xl mx-auto p-5 items-center justify-center flex gap-10">
+          <div className="text-white">
+            <h1 className='text-2xl font-bold'>500K+ More</h1>
+            <p className='text-sm text-slate-400'>Trusted Companies Partner</p>
+          </div>
+
+          <div className="text-white">
+            <h1 className='text-2xl font-bold'>300K+ View</h1>
+            <p className='text-sm text-slate-400'>Partner of training journey</p>
+          </div>
+
+          <div className="text-white">
+            <h1 className='text-2xl font-bold'>Fitness rooms</h1>
+            <p className='text-sm text-slate-400'>The best training place in downtown</p>
+          </div>
+
+          <div className="text-white">
+            <h1 className='text-2xl font-bold'>Awards </h1>
+            <p className='text-sm text-slate-400'>Gain an award of the trainer</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
