2c16bac800b4f337cf419b186891e5cdf77b7e6f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-08 12:52:46 +0200

    New code is added now

--- src/app/components/Heros.jsx
+++ src/app/components/Heros.jsx
@@ -17,7 +17,11 @@ export default function Heros() {
             <p>And mind through the power of fitness</p>
         </div>
         
-        
+        <div className="">
+            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-5'>
+                Get Started
+            </button>
+        </div>
     </div>
   )
 }
