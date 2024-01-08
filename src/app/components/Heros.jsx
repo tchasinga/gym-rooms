9b24e498a3960bc4e28014bd822d17bbacb188d9
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-08 13:18:30 +0200

    New code is added

--- src/app/components/Heros.jsx
+++ src/app/components/Heros.jsx
@@ -29,7 +29,7 @@ export default function Heros() {
              </div>
              <p className='text-gray-600 font-normal text-sm'>Watch Video</p>
            </div>
-
+           
         </div>
     </div>
   )
