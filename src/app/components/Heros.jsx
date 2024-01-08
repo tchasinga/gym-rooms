84ce93c441c1fc5239bc85552b9790e0f4777443
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-08 11:42:32 +0200

    New code

--- src/app/components/Heros.jsx
+++ src/app/components/Heros.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export default function Heros() {
   return (
     <div >
-      
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,11 @@
 "use client"
 import Image from 'next/image'
-import Heros from './components/Heros'
+
 
 export default function Home() {
   return (
     <main className="">
-       <section>
-         <Heros />
-       </section>
+      
     </main>
   )
 }
