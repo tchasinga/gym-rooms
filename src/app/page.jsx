64511e4f1be53f672f366c1d27083258f46f2535
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-08 11:35:23 +0200

    New code is good

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,13 @@
 "use client"
 import Image from 'next/image'
+import Heros from './components/Heros'
 
 export default function Home() {
   return (
     <main className="">
+       <section>
+         <Heros />
+       </section>
     </main>
   )
 }
