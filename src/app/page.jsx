7bbfb178f9e54817f69929591259188025268948
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-08 16:18:32 +0200

    new code is added now

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,14 @@
 "use client"
 import Image from 'next/image'
+import Companies from './components/Companies'
 
 
 export default function Home() {
   return (
     <main className="">
-      
+       <section>
+         <Companies />  
+       </section>
     </main>
   )
 }
