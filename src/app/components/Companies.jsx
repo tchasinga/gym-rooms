7358a404d7e4609661ef39f5d293c63ebece026a
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-08 17:31:58 +0200

    New code is added

--- src/app/components/Companies.jsx
+++ src/app/components/Companies.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Image from "next/image";
 import ketut from '../img/pexels-ketut-subiyanto-4720831.jpg'
+import ImageIcon from '../img/6741796.png'
 
 export default function Companies() {
   return (
@@ -34,8 +35,12 @@ export default function Companies() {
         <div className="max-w-6xl mx-auto mt-10">
 
             <div className="relative">
-                <div className='w-1/2'>
-                    <Image src={ketut} className='' />
+                <div className='w-1/2 rounded-2xl'>
+                    <Image src={ketut} className='rounded-2xl' />
+                </div>
+                <div className='flex items-center bg-slate-200 w-44 py-2 px-1 rounded-2xl'>
+                    <Image src={ImageIcon} className='h-8 w-8 text-red-800'/>
+                    <p className='text-sm'>Professional Trainer</p>
                 </div>
             </div>
 
