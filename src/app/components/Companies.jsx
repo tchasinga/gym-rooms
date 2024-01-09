93d4c40994783008b26fbb55f32c87b565676887
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-09 10:07:59 +0200

    New code is added

--- src/app/components/Companies.jsx
+++ src/app/components/Companies.jsx
@@ -36,9 +36,9 @@ export default function Companies() {
 
             <div className="relative  w-2/3 flex-1 ">
                 <div className='myWidth rounded-2xl'>
-                    <Image src={ketut} className='rounded-2xl border' />
+                    <Image src={ketut} className='rounded-2xl' />
                 </div>
-                <div className='flex border items-center myblur w-44 py-2 px-1 rounded-2xl absolute top-[60%] left-[69%]'>
+                <div className='flex items-center myblur w-44 py-2 px-1 rounded-2xl absolute top-[60%] left-[69%]'>
                     <Image src={ImageIcon} className='h-8 w-8 text-red-800'/>
                     <p className='text-sm'>Professional Trainer</p>
                 </div>
