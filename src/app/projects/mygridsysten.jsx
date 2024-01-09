4470264c8d48283eba493d521540194e643acb17
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-09 11:44:33 +0200

    new code is aded

--- src/app/projects/mygridsysten.jsx
+++ src/app/projects/mygridsysten.jsx
@@ -9,19 +9,19 @@ export const myObject = [
     {
         icon: <MdOutlineFitnessCenter />,
         title: "Cardio Training",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates!",
+        text: "Our trainers will design that a progressive workout plans that proper achive gains strength",
         arrow : <IoIosArrowRoundForward />
     },
     {
         icon: <GrYoga />,
         title: "Yoga Pilates",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates!",
+        text: "This program combines yoga with cardio & strength training to help you lose weight & tone up",
         arrow : <IoIosArrowRoundForward />
     },
     {
         icon: <GiEnergyArrow />,
         title: "Energy Training",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates!",
+        text: "",
         arrow : <IoIosArrowRoundForward />
     },
     {
