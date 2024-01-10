fce29eb242ba64345ccb984a8d445cc37a07843f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-10 13:56:27 +0200

    New code is added

--- src/app/components/Bestplan.jsx
+++ src/app/components/Bestplan.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { GiCheckMark } from 'react-icons/gi'
+import { GiCheckMark } from 'react-icons/gi'
 
 export default function Bestplan() {
   return (
@@ -10,9 +10,119 @@ export default function Bestplan() {
          </div>
 
         <div className="">
-            <div className="flex justify-between items-center mt-10">
-                <div className="bg-red-900 w-1/4 p-5 rounded-lg">
+            <div className="flex items-center mt-10 justify-center gap-5">
+                
+                <div className="bg-red-900 w-1/4 h-80 rounded-lg p-5 flex flex-col text-center">
+                <h3 className='uppercase font-light text-xs text-slate-200'>discover</h3>
+                    <div className="flex items-center gap-1 text-white pt-3 pb-3 justify-center">
+                        <h2 className='font-bold text-2xl'>$99</h2>
+                        <p className='text-xs text-gray-400'>/Per Month</p>
+                    </div>
+                   
+                    <div className="flex flex-col justify-center items-center w-full">
+                    <div className="pt-4 flex items-center justify-center gap-1">
+                        <GiCheckMark className='text-white text-xs'/>
+                        <p className='text-white text-xs'>5 classes per month</p>
+                    </div>
+
+                    <div className="pt-4 flex items-center justify-center gap-1">
+                        <GiCheckMark className='text-white text-xs'/>
+                        <p className='text-white text-xs'>4 group class monthly</p>
+                    </div>
+
+                    <div className="pt-4 flex items-center justify-center gap-1">
+                        <GiCheckMark className='text-xs'/>
+                        <p className='text-xs'>E-book fitness guide</p>
+                    </div>
+
+                     <div className="pt-4 flex items-center justify-center gap-1">
+                        <GiCheckMark className='text-xs'/>
+                        <p className='text-xs'>E-book fitness guide</p>
+                    </div>
+                    </div>
+                    
+                    <div className="rounded-full justify-center flex  border items-center text-center relative bottom-[-10%] left-4 right-0  py-3 text-white w-[90%]">
+                        <div className="">
+                          <h3>Choose Plan</h3>
+                        </div>
+                    </div>
                 </div>
+
+                <div className="bg-red-900 w-1/4 h-[370px] rounded-lg p-5 flex flex-col text-center">
+                <h3 className='uppercase font-light text-xs text-slate-200'>discover</h3>
+                    <div className="flex items-center gap-1 text-white pt-3 pb-3 justify-center">
+                        <h2 className='font-bold text-2xl'>$299</h2>
+                        <p className='text-xs text-gray-400'>/Per Month</p>
+                    </div>
+                   
+                    <div className="flex flex-col justify-center items-center w-full">
+                    <div className="pt-4 flex items-center justify-center gap-1">
+                        <GiCheckMark className='text-white text-xs'/>
+                        <p className='text-white text-xs'>10 classes per month</p>
+                    </div>
+
+                    <div className="pt-4 flex items-center justify-center gap-1">
+                        <GiCheckMark className='text-white text-xs'/>
+                        <p className='text-white text-xs'>8 group class monthly</p>
+                    </div>
+
+                    <div className="pt-4 flex items-center justify-center gap-1">
+                        <GiCheckMark className='text-xs'/>
+                        <p className='text-xs'>Online class access</p>
+                    </div>
+
+                     <div className="pt-4 flex items-center justify-center gap-1">
+                        <GiCheckMark className='text-xs'/>
+                        <p className='text-xs'>E-book fitness guide</p>
+                    </div>
+
+                    <div className="pt-4 flex items-center justify-center gap-1">
+                        <GiCheckMark className='text-xs'/>
+                        <p className='text-xs'>7 Extra fitness training</p>
+                    </div>
+
+                    </div>
+                    
+                    <div className="rounded-full justify-center flex  border items-center text-center relative bottom-[-10%] left-4  right-0  py-3 text-white w-[90%]">
+                        <h3>Choose Plan</h3>
+                    </div>
+                </div>
+
+
+                <div className="bg-red-900 w-1/4 h-80 rounded-lg p-5 flex flex-col text-center">
+                    <h3 className='uppercase font-light text-sm text-slate-200'>discover</h3>
+                    <div className="flex items-center gap-1 text-white pt-3 pb-3 justify-center">
+                        <h2 className='font-bold text-2xl'>$99</h2>
+                        <p className='text-xs text-gray-400'>/Per Month</p>
+                    </div>
+                   
+                    <div className="flex flex-col justify-center items-center w-full">
+                    <div className="pt-4 flex items-center justify-center gap-1">
+                        <GiCheckMark className='text-white text-xs'/>
+                        <p className='text-white text-xs'>5 classes per month</p>
+                    </div>
+
+                    <div className="pt-4 flex items-center justify-center gap-1">
+                        <GiCheckMark className='text-white text-xs'/>
+                        <p className='text-white text-xs'>4 group class monthly</p>
+                    </div>
+
+                    <div className="pt-4 flex items-center justify-center gap-1">
+                        <GiCheckMark className='text-xs'/>
+                        <p className='text-xs'>E-book fitness guide</p>
+                    </div>
+
+                     <div className="pt-4 flex items-center justify-center gap-1">
+                        <GiCheckMark className='text-xs'/>
+                        <p className='text-xs'>E-book fitness guide</p>
+                    </div>
+                    </div>
+                    
+                    <div className="rounded-full justify-center flex  border items-center text-center relative bottom-[-10%] left-4 right-0  py-3 text-white w-[90%]">
+                        <h3>Choose Plan</h3>
+                    </div>
+                </div>
+
                 </div> 
         </div>
     </div>
