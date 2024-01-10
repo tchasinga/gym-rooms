35878e5ae3f5210ac0be4bd031acc8c5c3412f03
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-10 12:53:43 +0200

    new code is now added

--- src/app/components/Bestplan.jsx
+++ src/app/components/Bestplan.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function Bestplan() {
   return (
-    <div className='max-w-6xl mx-auto'>
-        
+    <div className='max-w-6xl mx-auto p-10'>
+         <div className="text-center flex-col flex w-full">
+            <h1 className='text-white text-2xl font-semibold mb-2'>Choose the best plan</h1>
+            <p className='text-sm text-slate-200 font-normal'>Choose a plan that's rigth for your growing team. simple picing & no hiddeen charge</p>
+         </div>
+
+        <div className="">
+            
+        </div>
+
     </div>
   )
 }
