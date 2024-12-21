12a3ac1f2a149f1d1bcfd55ffebf766055738ce3
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-21 17:04:30 +0300

    refactor page component for improved formatting and consistency

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,37 +1,34 @@
-"use client"
+"use client";
 
-import Companies from './components/Companies'
-import BestOffer from './components/BestOffer'
-import Healthstate from './components/Healthstate'
-import Bestplan from './components/Bestplan'
-import MyFooter from './components/MyFooter'
-import Heros from './components/Heros'
-
+import Companies from "./components/Companies";
+import BestOffer from "./components/BestOffer";
+import Healthstate from "./components/Healthstate";
+import Bestplan from "./components/Bestplan";
+import MyFooter from "./components/MyFooter";
+import Heros from "./components/Heros";
 
 export default function Home() {
   return (
     <main className="">
+      <Heros />
 
-      <Heros/>
-     
-         <Companies />  
-       
+      <Companies />
 
-       <div className='NewCodeIsnow '>
-       <BestOffer />
-       </div>
+      <div className="NewCodeIsnow ">
+        <BestOffer />
+      </div>
 
-       <section className='Healthstate'>
+      <section className="Healthstate">
         <Healthstate />
-       </section>
+      </section>
 
-       <div className='mynewone'>
-          <Bestplan />
-        </div>
+      <div className="mynewone">
+        <Bestplan />
+      </div>
 
-        <div className="MyFooter">
-         <MyFooter />
-       </div> 
+      <div className="MyFooter">
+        <MyFooter />
+      </div>
     </main>
-  )
+  );
 }
