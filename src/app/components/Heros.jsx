4d4298f0df184e3f1a6ee14bd435818fb08dd451
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-21 17:11:58 +0300

    adjust subheading position in Heros component for improved layout

--- src/app/components/Heros.jsx
+++ src/app/components/Heros.jsx
@@ -71,7 +71,7 @@ export default function Heros() {
       </div>
       <div
         ref={subHeadingRef}
-        className="absolute bottom-10 left-[65%] text-white text-[8rem] font-bold uppercase  w-[20%] z-0"
+        className="absolute bottom-14 left-[65%] text-white text-[8rem] font-bold uppercase  w-[20%] z-0"
       >
         <h1>last</h1>
       </div>
