81a5890ae94534c166ac1958a7f61e85dfffb241
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-21 14:21:43 +0300

    update Bestplan and Companies components with new styles and custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        customGray: '#38393d', // Add your custom color here
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -15,4 +18,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
