32ec8e8518c9d9b2843e9250c94a646d8f0ae4d7
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-21 17:00:34 +0300

    remove unused Image import from page component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import Image from 'next/image'
+
 import Companies from './components/Companies'
 import BestOffer from './components/BestOffer'
 import Healthstate from './components/Healthstate'
